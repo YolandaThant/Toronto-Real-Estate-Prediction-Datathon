e4dc5460339af476cc58eb73139d0d8d299a7d8a
YolandaThant <yolanda.lee159@gmail.com>
2025-03-01 14:05:59 -0500

    added some code changes

--- Visualization1.py
+++ Visualization1.py
@@ -18,16 +18,16 @@ def convert_size(value):
     if '-' in value:  # If it's a range, calculate the median
         low, high = map(int, value.split('-'))
         return (low + high) // 2
-    elif '+' in value:  # If there's a '+' sign, remove it and use the base value
-        return int(value.replace('+', ''))
-    else:  # If it's a single number, convert directly
-        return int(value)
+    # elif '+' in value:  # If there's a '+' sign, remove it and use the base value
+    #     return int(value.replace('+', ''))
+    # else:  # If it's a single number, convert directly
+    #     return int(value)
 
 # Apply function to 'size' column
 dataset['size'] = dataset['size'].apply(convert_size)
 
 # Print the entire updated dataset
-# print(dataset)
+print(dataset)
 
 # (Optional) Save the cleaned dataset back to a CSV file
 dataset.to_csv("cleaned_real_estate_data.csv", index=False)
