03d323058c51f272a029d15b92780e83281adbf0
YolandaThant <yolanda.lee159@gmail.com>
2025-03-01 17:53:28 -0500

    i

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    elif '+' in value:  # If there's a '+' sign, remove it and use the base value
-        return int(value.replace('+', ''))
-    else:  # If it's a single number, convert directly
-        return int(value)
