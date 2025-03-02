42b7991ea835b464a782aac90ab27b86b9298adc
YolandaThant <yolanda.lee159@gmail.com>
2025-03-01 23:32:32 -0500

    added spacing

--- JointPlot.py
+++ JointPlot.py
@@ -1,8 +1,7 @@
-import seaborn as sns 
-import matplotlib.pyplot as plt 
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-
 
 # read the data from the dataset
 data = pd.read_csv("cleaned_real_estate_data.csv")
@@ -14,12 +13,12 @@ size = df['size']
 summary = size.describe()
 print(summary)
 # plot the graph
-graph = sns.jointplot(x = "size", y = "price (In 1000s)", 
-              data = df, kind = "reg", scatter_kws={'color': 'limegreen'}, line_kws={'color': 'darkgreen', 'linewidth': 2}, marginal_kws={'color': 'green'}) 
+graph = sns.jointplot(x="size", y="price (In 1000s)", data=df, kind="reg",
+                      scatter_kws={'color': 'limegreen'}, line_kws={'color': 'darkgreen', 'linewidth': 2},
+                      marginal_kws={'color': 'green'})
 
 # title of the figure
-graph.figure.suptitle("Joint plot for size (square feet) vs Price", fontsize = 12)
+graph.figure.suptitle("Joint plot for size (square feet) vs Price", fontsize=12)
 
-
-# show the plot 
-plt.show() 
+# show the plot
+plt.show()
