64d68858d772a80f8bc0d8bc6e63f2a2c1b53e8a
25hiya <157934130+25hiya@users.noreply.github.com>
2025-03-01 21:12:34 -0500

    Changes

--- JointPlot.py
+++ JointPlot.py
@@ -1,6 +1,8 @@
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 
 # read the data from the dataset
 data = pd.read_csv("cleaned_real_estate_data.csv")
@@ -8,12 +10,16 @@ data = pd.read_csv("cleaned_real_estate_data.csv")
 # make it into a dataframe
 df = pd.DataFrame(data)
 
+size = df['size']
+summary = size.describe()
+print(summary)
 # plot the graph
 graph = sns.jointplot(x = "size", y = "price (In 1000s)", 
               data = df, kind = "reg", scatter_kws={'color': 'limegreen'}, line_kws={'color': 'darkgreen', 'linewidth': 2}, marginal_kws={'color': 'green'}) 
 
 # title of the figure
-graph.figure.suptitle("Size (square feet) vs Price", fontsize = 12)
+graph.figure.suptitle("Joint plot for size (square feet) vs Price", fontsize = 12)
+
 
 # show the plot 
 plt.show() 
